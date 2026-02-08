e144ccd88e6087440ac5fafdf44b2dab2db772aa
AshmiC-JetLearner <ashmi.c32@gmail.com>
2026-02-08 12:49:44 -0500

    Quiz.py

--- Quiz.py
+++ Quiz.py
@@ -1,7 +1,7 @@
 import pgzrun
 WIDTH=800
 HEIGHT=700
-title='Quiz Master'
+TITLE='Quiz Master'
 
 marquee_box=Rect(0,0 ,800,60 )#Rect(ini_x,ini_y,w,h)
 question_box=Rect(20,80 ,500,140)
@@ -46,7 +46,7 @@ def draw():
     screen.draw.textbox('SKIP',skip_box,color='white')
 
     #question box text
-    screen.draw.textbox(question[0].strip(),question_box,color="white"shadow=(0.5,0,5),
+    screen.draw.textbox(question[0].strip(),question_box,color="white",shadow=(0.5,0,5),
                         scolor="dim grey")
     
     #display answer box
@@ -54,6 +54,75 @@ def draw():
     for answer_box in answer_boxes:
         screen.draw.textbox(question[index].strip(),answer_box,color="black")
         index=index+1
+
+def update():
+    move_marquee()
+
+
+def move_marquee():
+    marquee_box.x -= 2
+    if marquee_box.right < 0:
+        marquee_box.left=WIDTH
+
+question_file_name= 'Quiz master/questions.txt'
+questions=[]
+question_count=0
+question_index=0
+
+def read_question_file():
+    global question_count
+    global questions
+    q_file=open(question_file_name, 'r')
+    for question in q_file:
+        questions.append(question)
+        question_count=question_count+1
+    q_file.close()
+
+def read_next_q():
+    global question_index
+    question_index += 1
+    return questions.pop(0).split("|")
+
+def on_mouse_down(pos):
+    index=1
+    for box in answer_boxes:
+        if box.collidepoint(pos):
+            if index ==int(question[5]):
+                correct_answer()
+
+            else:
+                game_over()
+        index += 1
+    if  skip_box.collidepoint(pos):
+        skip_question()
+
+def correct_answer():
+    global score,questions,time_left, question
+    score +=1
+    if questions:
+        question=read_next_q
+        time_left=10
+    else:
+        game_over()
+    
+def game_over():
+    global question, time_left, is_game_over
+    message=f'Game over!\n You got {score} questions correct!'
+    question=[message, "-","-","-","-","-",5]
+    time_left=0
+    game_over=True
+     
+def update_time_left():
+    global time_left
+    if time_left:
+        time_left=time_left-1
+    else:
+        game_over()
+
+read_question_file()
+question=read_next_q
+clock.schedule_interval(update_time_left, 1)
+
 
 
 
